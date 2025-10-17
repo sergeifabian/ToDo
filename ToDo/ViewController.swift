b3589779102d85b38c9d3b9473c73f5c5f91abf7
Sergei Fabian <me.sergeifabian@gmail.com>
2025-10-18 01:15:45 +0300

    Initial commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Sergei Fabian on 18.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
