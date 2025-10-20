1c8bfd8589df855a0ccebd83aeeb1f4919aa85dd
Sergei Fabian <me.sergeifabian@gmail.com>
2025-10-20 16:16:34 +0300

    Adds common type aliases

---
+++ ToDo/Sources/Shared/Utils/Types/Types.swift
@@ -0,0 +1,12 @@
+//
+//  Types.swift
+//  ToDo
+//
+//  Created by Sergei Fabian on 20.10.2025.
+//
+
+import UIKit
+
+typealias EmptyClosure = () -> Void
+typealias TypeClosure<T> = (T) -> Void
+typealias ResultClosure<T> = (Result<T, Error>) -> Void
