59a49cdecb86f37bc18f6ddd7ec3fcab2404643e
Sergei Fabian <me.sergeifabian@gmail.com>
2025-10-20 16:25:10 +0300

    Adds UIImage extension for system symbols

---
+++ ToDo/Sources/Shared/Utils/Extensions/UIImage+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+Ext.swift
+//  ToDo
+//
+//  Created by Sergei Fabian on 20.10.2025.
+//
+
+import UIKit
+
+extension UIImage {
+    enum Symbol {
+        static let circle = UIImage(systemName: "circle")
+        static let checkmarkCircle = UIImage(systemName: "checkmark.circle")
+        static let squareAndPencil = UIImage(systemName: "square.and.pencil")
+        static let trash = UIImage(systemName: "trash")
+    }
+}
